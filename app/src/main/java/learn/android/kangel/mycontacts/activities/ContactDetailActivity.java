b52b49b6f0177ebfc82561ed61d61e9bff112e58
Kangel Zenn <cookieyeah1@gmail.com>
2016-03-26 15:50:40 +0800

    "before using viewHolder for the contact detail row"

--- app/src/main/java/learn/android/kangel/mycontacts/activities/ContactDetailActivity.java
+++ app/src/main/java/learn/android/kangel/mycontacts/activities/ContactDetailActivity.java
@@ -1,33 +1,43 @@
 package learn.android.kangel.mycontacts.activities;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v4.content.ContextCompat;
+import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.text.ClipboardManager;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.view.ViewStub;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import learn.android.kangel.mycontacts.R;
 
 /**
  * Created by Kangel on 2016/3/24.
  */
-public class ContactDetailActivity extends AppCompatActivity implements android.support.v4.app.LoaderManager.LoaderCallbacks<Cursor> {
+public class ContactDetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>, View.OnClickListener {
     private LinearLayout container;
     private CollapsingToolbarLayout collapsingToolbarLayout;
     private final static int DETAILS_QUERY_ID = 0;
     private final static int NAME_QUERY_ID = 1;
     private final static int EMAIL_QUERY_ID = 2;
     private final static int PHONE_QUERY_ID = 3;
+    private final static String TAG_PHONE = "PHONE";
+    private final static String TAG_EMAIL = "EMAIL";
     private static final String[] PROJECTION =
             new String[]{
                     ContactsContract.Data._ID,
@@ -137,54 +147,57 @@ public class ContactDetailActivity extends AppCompatActivity implements android.
                 break;
             }
             case PHONE_QUERY_ID: {
-                if (data != null) {
-                    synchronized (this) {
-                        for (int i = 0; i < data.getCount(); i++) {
-                            data.moveToPosition(i);
-                            View v = LayoutInflater.from(this).inflate(R.layout.item_contact_info, container, false);
-                            TextView infoText = (TextView) v.findViewById(R.id.info_text);
-                            TextView hintText = (TextView) v.findViewById(R.id.hint_text);
-                            ImageView indicateIcon = (ImageView) v.findViewById(R.id.indicate_icon);
-                            ImageView action = (ImageView) v.findViewById(R.id.action_icon);
-                            action.setVisibility(View.VISIBLE);
-                            if (i == 0) {
-                                indicateIcon.setImageResource(R.drawable.ic_call_black_24dp);
-                            }
-                            action.setImageResource(R.drawable.ic_message_black_24dp);
-                            infoText.setText(data.getString(DATA1_INDEX));
-                            int type = data.getInt(DATA2_INDEX);
-                            CharSequence typeString = ContactsContract.CommonDataKinds.Phone.getTypeLabel(getResources(), type, data.getString(DATA3_INDEX));
-                            hintText.setText(typeString);
-                            container.addView(v);
+                if (data != null && data.getCount() > 0) {
+                    ViewStub viewStub = (ViewStub) findViewById(R.id.phone_view_stub);
+                    View panel = viewStub.inflate();
+                    LinearLayout container = (LinearLayout) panel.findViewById(R.id.container);
+                    for (int i = 0; i < data.getCount(); i++) {
+                        data.moveToPosition(i);
+                        View v = LayoutInflater.from(this).inflate(R.layout.item_contact_info, container, false);
+                        TextView infoText = (TextView) v.findViewById(R.id.info_text);
+                        TextView hintText = (TextView) v.findViewById(R.id.hint_text);
+                        ImageView indicateIcon = (ImageView) v.findViewById(R.id.indicate_icon);
+                        ImageView action = (ImageView) v.findViewById(R.id.action_icon);
+                        action.setVisibility(View.VISIBLE);
+                        if (i == 0) {
+                            indicateIcon.setImageResource(R.drawable.ic_call_black_24dp);
                         }
+                        action.setImageResource(R.drawable.ic_message_black_24dp);
+                        infoText.setText(data.getString(DATA1_INDEX));
+                        int type = data.getInt(DATA2_INDEX);
+                        CharSequence typeString = ContactsContract.CommonDataKinds.Phone.getTypeLabel(getResources(), type, data.getString(DATA3_INDEX));
+                        hintText.setText(typeString);
+                        v.setOnClickListener(this);
+                        action.setOnClickListener(this);
+                        action.setTag(infoText.getText()); //put number which is used to send sms in the actionButton
+                        v.setTag(TAG_PHONE);
+                        registerForContextMenu(v);
+                        container.addView(v);
                     }
                 }
                 break;
             }
             case EMAIL_QUERY_ID: {
-                if (data != null) {
-                    synchronized (this) {
-                        for (int i = 0; i < data.getCount(); i++) {
-                            data.moveToPosition(i);
-                            View v = LayoutInflater.from(this).inflate(R.layout.item_contact_info, container, false);
-                            TextView infoText = (TextView) v.findViewById(R.id.info_text);
-                            TextView hintText = (TextView) v.findViewById(R.id.hint_text);
-                            ImageView indicateIcon = (ImageView) v.findViewById(R.id.indicate_icon);
-                            if (i == 0) {
-                                indicateIcon.setImageResource(R.drawable.ic_email_black_24dp);
-                                if (container.getChildCount() > 0) {
-                                    View divider = new View(this);
-                                    divider.setBackgroundColor(ContextCompat.getColor(this, R.color.divider));
-                                    divider.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 1));
-                                    container.addView(divider);
-                                }
-                            }
-                            infoText.setText(data.getString(DATA1_INDEX));
-                            int type = data.getInt(DATA2_INDEX);
-                            CharSequence typeString = ContactsContract.CommonDataKinds.Email.getTypeLabel(getResources(), type, data.getString(DATA3_INDEX));
-                            hintText.setText(typeString);
-                            container.addView(v);
+                if (data != null && data.getCount() > 0) {
+                    ViewStub viewStub = (ViewStub) findViewById(R.id.email_view_stub);
+                    View panel = viewStub.inflate();
+                    LinearLayout container = (LinearLayout) panel.findViewById(R.id.container);
+                    for (int i = 0; i < data.getCount(); i++) {
+                        data.moveToPosition(i);
+                        View v = LayoutInflater.from(this).inflate(R.layout.item_contact_info, container, false);
+                        TextView infoText = (TextView) v.findViewById(R.id.info_text);
+                        TextView hintText = (TextView) v.findViewById(R.id.hint_text);
+                        ImageView indicateIcon = (ImageView) v.findViewById(R.id.indicate_icon);
+                        if (i == 0) {
+                            indicateIcon.setImageResource(R.drawable.ic_email_black_24dp);
                         }
+                        infoText.setText(data.getString(DATA1_INDEX));
+                        int type = data.getInt(DATA2_INDEX);
+                        CharSequence typeString = ContactsContract.CommonDataKinds.Email.getTypeLabel(getResources(), type, data.getString(DATA3_INDEX));
+                        hintText.setText(typeString);
+                        v.setTag(TAG_EMAIL);
+                        registerForContextMenu(v);
+                        container.addView(v);
                     }
                 }
                 break;
@@ -198,4 +211,66 @@ public class ContactDetailActivity extends AppCompatActivity implements android.
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.info_item: {
+                String tag = (String) v.getTag();
+                switch (tag) {
+                    case TAG_PHONE:
+                        TextView infoText = (TextView) v.findViewById(R.id.info_text);
+                        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + infoText.getText()));
+                        startActivity(intent);
+                        break;
+                }
+                break;
+            }
+            case R.id.action_icon: {
+                String number = (String) v.getTag();
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms", number, null)));
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        switch ((String)v.getTag()) {
+            case TAG_PHONE:
+                inflater.inflate(R.menu.menu_context_phone, menu);
+                break;
+            case TAG_EMAIL:
+                inflater.inflate(R.menu.menu_context_email, menu);
+                break;
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.copy:
+                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                TextView infoText = (TextView) info.targetView.findViewById(R.id.info_text);
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                clipboard.setText(infoText.getText().toString());
+                Toast.makeText(ContactDetailActivity.this, getString(R.string.copy_successfully), Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+
+    class ViewHolder {
+        TextView infoText;
+        TextView hintText;
+        ImageView indicateIcon;
+        ImageView actionButton;
+        ViewHolder(View v){
+            TextView infoText = (TextView) v.findViewById(R.id.info_text);
+            TextView hintText = (TextView) v.findViewById(R.id.hint_text);
+            ImageView indicateIcon = (ImageView) v.findViewById(R.id.indicate_icon);
+            ImageView action = (ImageView) v.findViewById(R.id.action_icon);
+        }
+
+    }
 }
